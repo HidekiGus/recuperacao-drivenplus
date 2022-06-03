106f04d2bcf0964c8e3bddf06d66cd5bd5edf86e
Gustavo Hideki <40206355+HidekiGus@users.noreply.github.com>
2022-06-03 07:20:33 +0000

    Finaliza tela de escolha de planos

--- src/components/TelaPlanos.js
+++ src/components/TelaPlanos.js
@@ -10,6 +10,9 @@ import { useContext } from "react";
 export default function TelaPlanos() {
 
     const { token, setToken } = useContext(TokenContext);
+    const [ planos, setPlanos ] = useState([]);
+
+    useEffect(() => obtemPlanos(), []);
 
     function obtemPlanos() {
         let config = {
@@ -20,19 +23,34 @@ export default function TelaPlanos() {
 
         const promessa = axios.get("https://mock-api.driven.com.br/api/v4/driven-plus/subscriptions/memberships", config);
 
-        promessa.then((response) => console.log(response));
+        promessa.then((response) => setPlanos(response.data));
     }
-
-    obtemPlanos();
     
-
     return (
         <>
         <Tela>
+            <h1>Escolha seu Plano</h1>
+            <Planos>
+                {planos.length === 0 ? "" :
+                planos.map((plano, index) => <Plano key={index} imagem={plano.image} preco={plano.price} />
+                )}
+            </Planos>
         </Tela>
         </>
     );
+}
 
+function Plano({ imagem, preco }) {
+
+    let precoFormatado = preco.replace(".", ",");
+    precoFormatado = "R$ " + precoFormatado;
+
+    return(
+        <CardPlano>
+            <img src={imagem} />
+            <h1>{precoFormatado}</h1>
+        </CardPlano>
+    );
 }
 
 const Tela = styled.div`
@@ -43,4 +61,34 @@ const Tela = styled.div`
     align-items: center;
     flex-direction: column;
     justify-content: flex-start;
+
+    h1 {
+        font-family: 'Roboto';
+        font-size: 32px;
+        color: #FFFFFF;
+        margin: 3vh 0;
+    }
+`
+
+
+
+const Planos = styled.div`
+    width: 100vw;
+    height: 85vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+`
+
+const CardPlano = styled.div`
+    width: 80%;
+    height: 30%;
+    border: 3px solid #7e7e7e;
+    border-radius: 12px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: row;
 `
